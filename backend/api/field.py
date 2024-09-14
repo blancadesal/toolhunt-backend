fdf1d5639ac342e5d3b459e2d42d8a852bb8a64e
Slavina Stefanova <sstefanova@wikimedia.org>
2024-09-14 23:22:30 +0200

    field filter

--- backend/api/field.py
+++ backend/api/field.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from fastapi import APIRouter, HTTPException
 from tortoise.contrib.fastapi import HTTPNotFoundError
-from typing import List
 
 from backend.models.tortoise import Field
 
 router = APIRouter(prefix="/fields", tags=["fields"])
+
 
 @router.get(
     "/",
@@ -12,7 +14,7 @@ router = APIRouter(prefix="/fields", tags=["fields"])
     responses={404: {"model": HTTPNotFoundError}},
 )
 async def get_fields():
-    fields = await Field.all().values_list('name', flat=True)
+    fields = await Field.all().values_list("name", flat=True)
     if not fields:
         raise HTTPException(status_code=404, detail="No fields found")
     return list(fields)
