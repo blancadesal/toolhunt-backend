3568646f35c4544f97e41729b2ac9a2a2a4848a2
Slavina Stefanova <sstefanova@wikimedia.org>
2024-09-16 08:53:55 +0200

    return only active annotation fields

--- backend/api/field.py
+++ backend/api/field.py
@@ -3,9 +3,11 @@ from typing import List
 from fastapi import APIRouter, HTTPException
 from tortoise.contrib.fastapi import HTTPNotFoundError
 
+from backend.config import get_settings
 from backend.models.tortoise import Field
 
 router = APIRouter(prefix="/fields", tags=["fields"])
+settings = get_settings()
 
 
 @router.get(
@@ -14,7 +16,9 @@ router = APIRouter(prefix="/fields", tags=["fields"])
     responses={404: {"model": HTTPNotFoundError}},
 )
 async def get_fields():
-    fields = await Field.all().values_list("name", flat=True)
+    fields = await Field.filter(name__in=settings.active_annotations).values_list(
+        "name", flat=True
+    )
     if not fields:
         raise HTTPException(status_code=404, detail="No fields found")
     return list(fields)
