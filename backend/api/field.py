701b9175b2dcd510fab42ba581380106eb3f8b4a
Slavina Stefanova <sstefanova@wikimedia.org>
2024-09-16 09:12:32 +0200

    get fields from settings

--- backend/api/field.py
+++ backend/api/field.py
@@ -1,10 +1,7 @@
-from typing import List
-
 from fastapi import APIRouter, HTTPException
 from tortoise.contrib.fastapi import HTTPNotFoundError
 
 from backend.config import get_settings
-from backend.models.tortoise import Field
 
 router = APIRouter(prefix="/fields", tags=["fields"])
 settings = get_settings()
@@ -12,13 +9,11 @@ settings = get_settings()
 
 @router.get(
     "",
-    response_model=List[str],
+    response_model=list[str],
     responses={404: {"model": HTTPNotFoundError}},
 )
 async def get_fields():
-    fields = await Field.filter(name__in=settings.active_annotations).values_list(
-        "name", flat=True
-    )
+    fields = list(settings.active_annotations)
     if not fields:
         raise HTTPException(status_code=404, detail="No fields found")
-    return list(fields)
+    return fields
