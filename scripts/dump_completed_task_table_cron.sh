8f113595a5e1edbcd4e64a208a89efc92c9c0b58
Slavina Stefanova <sstefanova@wikimedia.org>
2024-10-10 07:51:11 +0200

    lint scripts

--- scripts/dump_completed_task_table_cron.sh
+++ scripts/dump_completed_task_table_cron.sh
@@ -5,7 +5,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
-cd $PROJECT_DIR
+cd "$PROJECT_DIR"
 
 docker compose exec -T db sh -c 'mysqldump -u $MARIADB_USER -p$MARIADB_PASSWORD web_prod completed_task' > ./completed_task_backup.sql
-
--- scripts/update_db_cron.sh
+++ scripts/update_db_cron.sh
@@ -4,9 +4,8 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
-cd $PROJECT_DIR
+cd "$PROJECT_DIR"
 
 docker compose exec -T web python scripts/update_db.py
-docker compose exec -T web cat db_update.log > db_update.log
+docker compose exec -T web cat db_update.log > ./db_update.log
 docker compose exec -T web rm db_update.log
-
